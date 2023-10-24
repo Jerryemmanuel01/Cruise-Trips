f782e9d4c767c61d457e1b9114453264a7e1530e
Jerryemmanuel01 <jerryemmanuelolisa@gmail.com>
2023-10-24 23:48:00 +0100

    mobile view adjusted

--- src/components/Carddetails/CardDetails.jsx
+++ src/components/Carddetails/CardDetails.jsx
@@ -77,7 +77,7 @@ const CardDetails = ({ places, loading, cityInfo }) => {
                     <div>
                       <MdLocationPin className="cardIcons locationIcon" />
                     </div>
-                    <div className="addressDetails">{place.address}</div>
+                    <div className="addressDetail">{place.address}</div>
                   </div>
                 )}
                 {place?.phone && (
